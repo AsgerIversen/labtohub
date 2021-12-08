2afc6cd6d744f1c26b2653ce5bc1c0f7795ca3ee
Asger Iversen <asger_iversen@keysight.com>
2021-12-09 00:41:07 +0100

    Add project files.

---
+++ LabToHub/Config.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabToHub
+{
+    static class Config
+    {
+        public const string GITLAB_ACCESS_TOKEN = "";
+        public const string GITHUB_ACCESS_TOKEN = "";
+
+        // Get this using:
+        //   var id = await github.Repository.Get("Keysight", "opentap").Id;  // keysight/opentap id = 436397521
+        public const long HUB_REPO_ID = 436397521;
+
+        public static Dictionary<string, string> LABEL_MAP = new Dictionary<string, string>
+        {
+            { "flow::To be Discussed", "To be Discussed" },
+            { "flow::Merge Requested", "Merge Requested" },
+            { "DOC", "documentation" }, // documentation is a default label in github
+        };
+
+    }
+}
---
+++ LabToHub/Program.cs
@@ -0,0 +1,64 @@
+﻿using GitLabApiClient;
+using Octokit;
+using System.Globalization;
+using LabToHub;
+
+// Get all issues from gitlab project
+var gitlab = new GitLabClient("https://gitlab.com", Config.GITLAB_ACCESS_TOKEN);
+var labIssues = await gitlab.Issues.GetAllAsync(10858059);
+labIssues = labIssues.OrderBy(x => x.Iid).ToList(); // add issue to github in the same order as they were added in gitlab
+
+var github = new GitHubClient(new ProductHeaderValue("LabToHub"));
+github.Credentials = new Credentials(Config.GITHUB_ACCESS_TOKEN);
+//var repo = await github.Repository.Get("Keysight", "opentap");  // id = 436397521
+
+var hubIssues = await github.Issue.GetAllForRepository(Config.HUB_REPO_ID, new RepositoryIssueRequest());
+
+
+var hubMilestones = await github.Issue.Milestone.GetAllForRepository(Config.HUB_REPO_ID);
+Dictionary<string,int> hubMilestoneId = hubMilestones.ToDictionary(m => m.Title, m => m.Number);
+
+CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+foreach (var li in labIssues)
+{
+    string migratedDescription = $"Originally filed {li.CreatedAt.ToString("MMMM dd yyyy")} by {li.Author.Name} on [GitLab]({li.WebUrl})" + Environment.NewLine + Environment.NewLine;
+    migratedDescription += li.Description;
+
+    // is this issue already in GitHub?
+    var hi = hubIssues.FirstOrDefault(i => i.Title == li.Title);
+    if (hi is not null)
+    {
+        if(hi.Body != migratedDescription)
+        {
+            var update = new IssueUpdate();
+            update.Body = migratedDescription;
+            await github.Issue.Update(Config.HUB_REPO_ID, hi.Number,update);
+            Console.WriteLine($"Updated issue decription for  {hi.Title}");
+        }
+        continue;
+    }
+
+    NewIssue newi = new NewIssue(li.Title);
+    newi.Body = migratedDescription;
+    li.Labels.ForEach(l => newi.Labels.Add(Config.LABEL_MAP.ContainsKey(l) ? Config.LABEL_MAP[l] : l ));
+    //i.Assignees.ForEach(a => newi.Assignees.Add(i.))
+    if (li.Milestone is not null)
+    {
+        if (!hubMilestoneId.ContainsKey(li.Milestone.Title))
+        {
+            NewMilestone newm = new NewMilestone(li.Milestone.Title);
+            newm.Description = li.Milestone.Description;
+            if(li.Milestone.DueDate is not null)
+                newm.DueOn = DateTimeOffset.Parse(li.Milestone.DueDate);
+            newm.State = li.Milestone.State == GitLabApiClient.Models.Milestones.Responses.MilestoneState.Closed ? ItemState.Closed : ItemState.Open;
+            var createdM = await github.Issue.Milestone.Create(Config.HUB_REPO_ID, newm);
+            Console.WriteLine($"Created milestone {createdM.Title}");
+            hubMilestoneId.Add(li.Milestone.Title, createdM.Number);
+        }
+        newi.Milestone = hubMilestoneId[li.Milestone.Title];
+    }
+
+
+    var createdI = await github.Issue.Create(Config.HUB_REPO_ID, newi);
+    Console.WriteLine($"Created issue {createdI.Title}");
+}
